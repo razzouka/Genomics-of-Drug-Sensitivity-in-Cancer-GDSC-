f27dce541a8e08fe0082ddcf23e0c0b1c43427da
Zeinab Razzouk <zeinab.razzouk@mail.ru>
2025-10-02 03:11:23 +0300

    Add data loader script

---
+++ scripts/data_loader.py
@@ -0,0 +1,9 @@
+﻿import pandas as pd
+import wget
+#URL_to_my_dataset
+FILE_ID = "1vsEQF1diDzx3ZFRYYzCPDXJC2bqpjWMV"
+file_url = f"https://docs.google.com/spreadsheets/d/{FILE_ID}/export?format=csv"
+#load_the_data
+raw_data = pd.read_csv(file_url)
+#show_the_first_10_rows
+print(raw_data.head(10))
